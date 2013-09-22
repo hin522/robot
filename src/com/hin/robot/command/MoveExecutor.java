02562df2120f80fa643b1b00f815884d7272c22b
Hin Lu <hlu4@yahoo.com>
2013-09-22 21:10:45 +1000

    updated MoveExecutor to check if robot would fall off table

--- src/com/hin/robot/command/MoveExecutor.java
+++ src/com/hin/robot/command/MoveExecutor.java
@@ -1,33 +1,71 @@
 package com.hin.robot.command;
 
+import com.hin.robot.model.Direction;
 import com.hin.robot.model.Position;
 import com.hin.robot.model.Robot;
+import com.hin.robot.model.Table;
 
 public class MoveExecutor extends Executor {
 
-	public MoveExecutor(Robot robot) {
+	private Table table;
+	
+	public MoveExecutor(Robot robot, Table table) {
 		super(robot);
+		this.table = table;
 	}
 
 	@Override
 	public void execute() {
+		if (!robot.isRobotOnTable()) {
+			return;
+		}
 		Position position = robot.getPosition();
-		switch (robot.getDirection()) {
+		if (!isFallFromTable(robot.getDirection(), position)) {
+			switch (robot.getDirection()) {
+			case EAST:
+				position.setCoordinateX(position.getCoordinateX() + 1);
+				break;
+			case SOUTH:
+				position.setCoordinateY(position.getCoordinateY() - 1);
+				break;
+			case WEST:
+				position.setCoordinateX(position.getCoordinateX() - 1);
+				break;
+			case NORTH:
+				position.setCoordinateY(position.getCoordinateY() + 1);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	private boolean isFallFromTable(Direction d, Position p) {
+		boolean result = true;
+		switch (d) {
 		case EAST:
-			position.setCoordinateX(position.getCoordinateX() + 1);
+			if (p.getCoordinateX() + 1 < table.getLengthX()) {
+				result = false;
+			}
 			break;
 		case SOUTH:
-			position.setCoordinateY(position.getCoordinateY() - 1);
+			if (p.getCoordinateY() - 1 >= 0) {
+				result = false;
+			}
 			break;
 		case WEST:
-			position.setCoordinateX(position.getCoordinateX() - 1);
+			if (p.getCoordinateX() - 1 >= 0) {
+				result = false;
+			}
 			break;
 		case NORTH:
-			position.setCoordinateY(position.getCoordinateY() + 1);
+			if (p.getCoordinateY() + 1 < table.getLengthY()) {
+				result = false;
+			}
 			break;
 		default:
 			break;
 		}
+		return result;
 	}
-
 }
--- test/com/hin/robot/command/MoveExecutorTest.java
+++ test/com/hin/robot/command/MoveExecutorTest.java
@@ -1,6 +1,7 @@
 package com.hin.robot.command;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,16 +9,67 @@ import org.junit.Test;
 import com.hin.robot.model.Direction;
 import com.hin.robot.model.Position;
 import com.hin.robot.model.Robot;
+import com.hin.robot.model.Table;
 
 public class MoveExecutorTest {
 
 	private Robot robot;
+	private Table table;
 	private MoveExecutor executor;
 	
 	@Before
 	public void setup() {
 		robot = new Robot();		
-		executor = new MoveExecutor(robot);
+		table = new Table(5, 5);
+		robot.setTable(table);
+		executor = new MoveExecutor(robot, table);
+	}
+	
+	@Test
+	public void testExecuteGivenRobotNotPlacedShouldDoNothing() {
+		robot.setTable(null);
+		executor.execute();
+		assertFalse(robot.isRobotOnTable());
+	}
+	
+	@Test
+	public void givenRobotOnEastTableEdgeFacingEastMoveShouldRemainAtSamePosition() {
+		robot.setPosition(new Position(4, 2));
+		robot.setDirection(Direction.EAST);
+		executor.execute();
+		assertEquals(Direction.EAST, robot.getDirection());
+		assertEquals(4, robot.getPosition().getCoordinateX());
+		assertEquals(2, robot.getPosition().getCoordinateY());	
+	}
+	
+	@Test
+	public void givenRobotOnSouthTableEdgeFacingSouthMoveShouldRemainAtSamePosition() {
+		robot.setPosition(new Position(2, 0));
+		robot.setDirection(Direction.SOUTH);
+		executor.execute();
+		assertEquals(Direction.SOUTH, robot.getDirection());
+		assertEquals(2, robot.getPosition().getCoordinateX());
+		assertEquals(0, robot.getPosition().getCoordinateY());	
+	}
+	
+	@Test
+	public void givenRobotOnWestTableEdgeFacingWestMoveShouldRemainAtSamePosition() {
+		robot.setPosition(new Position(0, 2));
+		robot.setDirection(Direction.WEST);
+		executor.execute();
+		assertEquals(Direction.WEST, robot.getDirection());
+		assertEquals(0, robot.getPosition().getCoordinateX());
+		assertEquals(2, robot.getPosition().getCoordinateY());	
+	}
+	
+	@Test
+	public void givenRobotOnNorthTableEdgeFacingNorthMoveShouldRemainAtSamePosition() {
+		robot.setPosition(new Position(2, 4));
+		robot.setDirection(Direction.NORTH);
+		executor.execute();
+		assertEquals(Direction.NORTH, robot.getDirection());
+		assertEquals(2, robot.getPosition().getCoordinateX());
+		assertEquals(4, robot.getPosition().getCoordinateY());	
 	}
 	
 	@Test
